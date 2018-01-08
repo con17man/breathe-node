fea1aabf90ed4e10c0347716f6caba8ce8ed5576
Constantin Radu <constantin.radu@maersk.com>
2018-01-08 03:36:22 +0200

    create basic structure for app @server side

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules/')));
+
+app.listen(PORT, function(){
+	console.log(`Up and running on PORT: ${PORT}`);
+});
+
+// connect to the mongoDB
+// mongoose.connect('mongodb://localhost/utilities', {
+//     useMongoClient: true
+// });
+
+// load the api
+apiController(app);
