efac21c2f061c3000c8da6587628b3734392bcfd
Constantin Radu <constantin.radu@maersk.com>
2018-05-06 11:24:20 +0300

    update login & register API methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,33 @@ const User = mongoose.model('User');
 
 exports.loginUser = async (req, res) => {
 
-    await User.findOne({ username: req.body.username }, (err, user) => {
+    await User.findOne({
+        username: req.body.username
+    }, (err, user) => {
         if (err) {
-            console.log('>>>error finding user', err);
+            console.log('login: error finding user', err);
         } else {
-            res.send(user);
+            // check if user exists
+            if (user) {
+                // check if password matches the one in DB
+                if (req.body.password === user.password) {
+                    res.send({
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        email: user.email
+                    });
+                } else {
+                    res.send({
+                        errorMsg: 'Wrong password. Please retry.'
+                    });
+                }
+            } else {
+                res.send({
+                    errorMsg: 'User does not exists. Try again or register.'
+                });
+            }
         }
-    })
+    });
 
 }
 
@@ -20,9 +40,23 @@ exports.registerUser = async (req, res) => {
     // [DONE] Save user input to DB
     // Check if email exists in the DB
 
-    const newUser = new User(req.body);
-    await newUser.save();
+    await User.findOne({
+        email: req.body.email
+    }, (err, user) => {
+        if (err) {
+            console.log('register: error finding email', err);
+        } else {
+            if (user && req.body.email === user.email) {
+                res.send({
+                    errorMsg: 'This email is already registered.'
+                });
+            } else {
+                const newUser = new User(req.body);
+                newUser.save();
 
-    res.send(req.body);
+                res.send(true);
+            }
+        }
+    });
 
 }
